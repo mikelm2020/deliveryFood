26d7a8d7d62df7637639192b4df5ba51ceaa1bb8
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2025-05-20 21:12:46 -0600

    Reestructured project

--- auth-service/bootstrap/app.php
+++ auth-service/bootstrap/app.php
@@ -14,9 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->alias([
-            "jwt.verify" => JwtMiddleware::class
-        ]);
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- auth-service/routes/api.php
+++ auth-service/routes/api.php
@@ -4,13 +4,21 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->middleware(['api', 'jwt.verify'])->group(function(){
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok', 'message' => 'Application is healthy']);
+});
+
+Route::prefix('v1')->group(function(){
     Route::prefix('auth')->group(function(){
         Route::post('login', [AuthController::class, 'login'])->name('login');
         Route::post('register', [AuthController::class, 'register']);
+    });
+});
+
+Route::prefix('v1')->middleware('jwt.auth')->group(function(){
+    Route::prefix('auth')->group(function(){
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
         Route::get('me', [AuthController::class, 'me']);
-
     });
 });
